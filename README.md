c1e6e6a1dc5044500e31370db3222e523f8fb46f
Nastya Grigoreva <grinastich@gmail.com>
2021-12-23 02:19:15 +0500

    tasks

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # refactoring17-2
+## Время выполнения файлов:
+### Время выполнения old_filter.py:
+![img_1.png](screens/oldfilterimg.png)
+### Время выполнения filter.py:
+![img.png](screens/filterimg.png)
+### Время выполнения filter_with_filename.py:
+![img.png](screens/filterwithimg.png)
+
+## Исходное изображение:
+![](img2.jpg)
+## Изображение после ***old_filter.py***:
+![img.png](screens/resimg1.png)
+## Изображение после ***filter_with_filename.py***:
+![img.png](screens/resimg1.png)
---
+++ filter_with_filename.py
@@ -0,0 +1,48 @@
+from PIL import Image
+import numpy as np
+
+
+def get_mosaic(image, size, gradation):
+    '''
+    Принимает изображение и применяет к нему фильтр, которые делает из неё чёрно-белый пиксель-арт
+
+    :param image: сходное изображение
+    :param size: размер мозаики
+    :param gradation: размер градации
+    :return: новое изображение
+    '''
+    image_arr = np.array(Image.open(image)).astype(int)
+    limit = 255 // gradation
+    image_len = len(image_arr)
+    image_h = len(image_arr[0])
+    i = 0
+    while i < image_len:
+        j = 0
+        while j < image_h:
+            segment = image_arr[i: i + size, j: j + size]
+            sum_c = np.sum(segment)
+            avg = int(sum_c // (size ** 2))
+            set_color(int(avg // limit) * limit / 3, image_arr, size, i, j)
+            j += size
+        i += size
+    return Image.fromarray(np.uint8(image_arr))
+
+
+def set_color(new_c, matrix, size, i, j):
+    """
+    Принимает размер мозаики и устанавливает новый цвет пикселя
+
+    :param new_c: новый цвет пикселя
+    :param matrix: матрица изоражения
+    :param size: размер мозаики
+    :param i: пиксль по оси x
+    :param j: пиксель по оси y
+    :return: устанавливает новый цвет пикселя
+    """
+    for x in range(i, i + size):
+        for y in range(j, j + size):
+            for z in range(3):
+                matrix[x][y][z] = new_c
+
+
+get_mosaic("img2.jpg", 10, 50).save('res1.jpg')
